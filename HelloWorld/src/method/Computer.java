a951be5920a7b955807be234427dc32cb0a957da
admin <admin@DESKTOP-8QCCD0E>
2025-03-14 17:51:17 +0900

    -

--- HelloWorld/src/method/Computer.java
+++ HelloWorld/src/method/Computer.java
@@ -2,7 +2,17 @@ package method;
 
 public class Computer {
 	
+	// 메소드 생성
 	int sum1(int[] values) {
+		int sum = 0;
+		for(int i = 0; i < values.length; i++) {
+			sum = sum + values[i];
+		}
+		return sum;
+	}
+	
+	// 메소드 생성
+	int sum2(int ...values) {
 		int sum = 0;
 		for(int i = 0; i < values.length; i++) {
 			sum = sum + values[i];
--- HelloWorld/src/method/ComputerExample.java
+++ HelloWorld/src/method/ComputerExample.java
@@ -4,12 +4,24 @@ public class ComputerExample {
 
 	public static void main(String[] args) {
 		
+		// 객체 생성
 		Computer myCom = new Computer();
 		
+		// 배열 선언
 		int[] valuse1 = {1, 2, 3};
-		myCom.sum1(valuse1);
-		myCom.sum1(new int[] {1, 2, 3});
-
+		
+		// 배열을 sum1의 매개값으로 할당해서 return 값을 result1에 저장
+		int result1 = myCom.sum1(valuse1);
+		System.out.println(result1);
+		
+		
+		// (이렇게 해도 됨)
+		int result2 = myCom.sum1(new int[] {1, 2, 3, 4, 5});
+		System.out.println(result2);
+		
+		// sum2
+		int result3 = myCom.sum2(1, 2, 3);//<<-- 결과는 똑같으나 매개값 표현 방식이 다름
+		System.out.println(result3);
 	}
 
 }
