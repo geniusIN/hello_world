30fb513a61551144211d5e2e656d058e5029f656
hoi <hoi@hiGram>
2025-04-03 22:24:27 +0900

    i

--- JSPPrj/src/main/java/com/newlecture/web/Calc2.java
+++ JSPPrj/src/main/java/com/newlecture/web/Calc2.java
@@ -9,13 +9,18 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet("/calc2")
 public class Calc2 extends HttpServlet{
 
 	@Override
-	public void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+	public void service(HttpServletRequest request
+						, HttpServletResponse response) throws ServletException, IOException{
+		
 		ServletContext application = request.getServletContext();
+		HttpSession session = request.getSession();
+		
 		response.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html; charset = UTF-8");
 		
@@ -31,10 +36,32 @@ public class Calc2 extends HttpServlet{
 		if(!v_.equals("")) {
 			v = Integer.parseInt(v_);
 		}
-			
-		application.setAttribute("value", v);
-		application.setAttribute("op", op);
 		
-	
+		
+		
+		if(op.equals("=")) {
+			
+//			int x = (int) application.getAttribute("value");
+			int x = (int) session.getAttribute("value");
+			int y = v;
+//			String operator = (String) application.getAttribute("op");
+			String operator = (String) session.getAttribute("op");
+			int result = 0;
+			
+			if(operator.equals("+")) {
+				result = x + y;
+				out.printf("result is %d\n", result);
+			}else {
+				result = x - y;
+				out.printf("result is %d\n", result);
+			}
+			
+		} else {
+//			application.setAttribute("value", v);
+//			application.setAttribute("op", op);
+			session.setAttribute("value", v);
+			session.setAttribute("op", op);
+		}
+		
 	}
 }
